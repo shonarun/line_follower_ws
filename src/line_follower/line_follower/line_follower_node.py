af501bc94abd48980dd9aa27d75468c3716f6871
Shon Arun <ajkallidukil8173@gmail.com>
2025-10-22 13:11:08 +0530

    Added a test launch file

--- src/line_follower/line_follower/line_follower_node.py
+++ src/line_follower/line_follower/line_follower_node.py
@@ -6,6 +6,11 @@ class LineFollowerNode(Node):
     def __init__(self):
         super().__init__('line_follower')
         self.get_logger().info('Line Follower Node has been started.')
+        self.declare_parameter("number", 2)
+        self.declare_parameter("publish_period", 1.0)
+        self.x = self.get_parameter("number").value
+        self.y = self.get_parameter("publish_period").value
+        self.get_logger().error(f"{self.x} : {self.y}")
 
 def main(args=None):
     rclpy.init(args=args)
